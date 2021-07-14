77aa37164d7f4b67d7b9e21f16ad94e4f477d50e
IefremovRoman <iefremov.roman@gmail.com>
2021-07-14 10:10:51 +0300

    flake8 clears everything

--- first_django_app/mysite/students/management/commands/generate_teachers.py
+++ first_django_app/mysite/students/management/commands/generate_teachers.py
@@ -1,9 +1,10 @@
 import json
 from random import choice
 
+from django.core.management.base import BaseCommand
+
 from faker import Faker
 
-from django.core.management.base import BaseCommand
 from students.models import Teacher
 
 
@@ -12,7 +13,8 @@ json_file = 'students/management/commands/university_subjects.json'
 
 
 class Command(BaseCommand):
-    help = 'Generate teachers'
+    def __init__(self):
+        self.help = 'Generate teachers'
 
     def add_arguments(self, parser):
         parser.add_argument('total', nargs='?', type=int)
@@ -26,12 +28,12 @@ class Command(BaseCommand):
             subjects = json.load(file)
 
         for i in range(count):
-            teacher = Teacher.objects.create(
-                                        first_name=faker.first_name(),
-                                        last_name=faker.last_name(),
-                                        age=faker.random_int(min=30, max=100),
-                                        subject=choice(subjects)
-                                        )
+            Teacher.objects.create(
+                                    first_name=faker.first_name(),
+                                    last_name=faker.last_name(),
+                                    age=faker.random_int(min=30, max=100),
+                                    subject=choice(subjects)
+                                    )
 
         message = f'{count} teacher(s) successfully created!'
-        self.stdout.write(self.style.SUCCESS(message)
+        self.stdout.write(self.style.SUCCESS(message))
